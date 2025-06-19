46dc50aa7032803e80910a0554bd9e8611044a77
sasha <sander_@inbox.ru>
2025-06-19 15:54:08 +0300

    added vies and templates

---
+++ hexlet_django_blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(
+        request,
+        'index.html',
+        context={
+            'word': 'added'
+        },
+    )
+
+def about(request):
+    return render(
+        request,
+        'about.html',
+        context={
+            'word': 'added'
+        },
+    )
